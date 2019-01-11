d51673715214df1d189a98cfccef6454199a1edf
Elise Phan <aliceconnic@gmail.com>
2019-01-11 18:52:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,6 +44,12 @@ class Dog
     dog
   end
   
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
   
   
   def self.find_or_create_by(name:, album:)
