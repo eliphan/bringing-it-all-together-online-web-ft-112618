c1dc24758c4cd3c8ad4c134f75f5300afe6cca0e
Elise Phan <aliceconnic@gmail.com>
2019-01-11 18:46:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,10 @@
 class Dog
   attr_accessor :name, :breed
   
-  def initalize
-    
+  def initialize(id: nil, name:, breed:)
+    # binding.pry
+    @id = id
+    @name = name
+    @breed = breed
   end
 end
